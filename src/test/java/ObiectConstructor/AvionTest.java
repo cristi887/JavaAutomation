b83a61f98e03ff3a522779f72cb25ca92dee9d91
Cristian Mircea Ursu <CristianMircea.Ursu@endava.com>
2025-05-03 17:39:22 +0300

    added * to end of each avion

--- src/test/java/ObiectConstructor/AvionTest.java
+++ src/test/java/ObiectConstructor/AvionTest.java
@@ -9,14 +9,17 @@ public class AvionTest {
         Avion avion1 = new Avion("pasageri","Boeing",
                 "B-127",260,"kerosen");
         avion1.prezentareAvion();
+        System.out.println("**********");
 
         Avion avion2 = new Avion("pasageri","Boeing",
                 "B-180",300,"eco");
         avion2.prezentareAvion();
+        System.out.println("**********");
 
         Avion avion3 = new Avion("pasageri","Airbus",
                 "A-180",280,"kerosen",50);
         avion3.prezentareAvion();
+        System.out.println("**********");
 
     }
 }
